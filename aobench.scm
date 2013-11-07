8cd9c364c51d9a530f6d2c91e14f501d2d7ff5fa
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-11-08 04:23:29 +0900

    [WIP] port to R7RS

--- aobench.scm
+++ aobench.scm
@@ -1,134 +1,237 @@
-#!r6rs
-; based on http://lucille.atso-net.jp/rwp/pt/
-(import (rnrs) (srfi :8) (srfi :27))
+(import (scheme base)
+	(scheme file)
+	(scheme write)
+	(scheme inexact)
+	(srfi 27))
 
-(define PI 3.141592)
+(define-syntax dotimes
+  (syntax-rules ()
+    ((dotimes (n count) body ...)
+     (do ((n 0 (+ n 1)))
+	 ((= n count))
+       body ...))))
+
+(define drand48 random-real)
+
+(define PI 3.14159265358979)
+
+(define WIDTH 256)
+(define HEIGHT 256)
+(define NSUBSAMPLES 2)
+(define NAO-SAMPLES 8)
+
+;;; vector-3d
 
 (define (vec x y z)
-  (let ((v (make-bytevector 24)))
-    (bytevector-ieee-double-native-set! v 0 x)
-    (bytevector-ieee-double-native-set! v 8 y)
-    (bytevector-ieee-double-native-set! v 16 z) v))
+  (let ((v (make-vector 3)))
+    (vector-set! v 0 x)
+    (vector-set! v 1 y)
+    (vector-set! v 2 z)
+    v))
+
 (define (vx x)
-  (bytevector-ieee-double-native-ref x 0))
+  (vector-ref x 0))
+
 (define (vy x)
-  (bytevector-ieee-double-native-ref x 8))
+  (vector-ref x 1))
+
 (define (vz x)
-  (bytevector-ieee-double-native-ref x 16))
-(define (vadd a b)
-  (vec (+ (vx a) (vx b)) (+ (vy a) (vy b)) (+ (vz a) (vz b))))
-(define (vsub a b)
-  (vec (- (vx a) (vx b)) (- (vy a) (vy b)) (- (vz a) (vz b))))
+  (vector-ref x 2))
+
+(define (v+ a b)
+  (vec (+ (vx a) (vx b))
+       (+ (vy a) (vy b))
+       (+ (vz a) (vz b))))
+
+(define (v- a b)
+  (vec (- (vx a) (vx b))
+       (- (vy a) (vy b))
+       (- (vz a) (vz b))))
+
+(define (vscale v x)
+  (vec (* x (vx v))
+       (* x (vy v))
+       (* x (vz v))))
+
 (define (vcross a b)
   (vec
     (- (* (vy a) (vz b)) (* (vy b) (vz a)))
     (- (* (vz a) (vx b)) (* (vz b) (vx a)))
     (- (* (vx a) (vy b)) (* (vx b) (vy a)))))
+
+(define (vdot a b)
+  (+ (* (vx a) (vx b))
+     (* (vy a) (vy b))
+     (* (vz a) (vz b))))
+
+(define (vlen a)
+  (sqrt
+   (+ (square (vx a))
+      (square (vy a))
+      (square (vz a)))))
+
+(define (vneg a)
+  (vec (- (vx a))
+       (- (vy a))
+       (- (vz a))))
+
 (define (vnormalize a)
   (let ((len (vlen a)))
     (if (> (abs len) 1.0e-6)
-      (let ((invlen (/ 1.0 len)))
-	(vec 
-	  (* (vx a) invlen)
-	  (* (vy a) invlen)
-	  (* (vz a) invlen))))))
-(define (square a)
-  (* a a))
-(define (vlen a)
-  (sqrt (+ (square (vx a)) (square (vy a)) (square (vz a)))))
-(define (vdot a b)
-  (+ (* (vx a) (vx b)) (* (vy a) (vy b)) (* (vz a) (vz b))))
-(define (vneg a)
-  (vec (- 0.0 (vx a)) (- 0.0 (vy a)) (- 0.0 (vz a))))
-(define (vscale v x)
-  (vec (* x (vx v)) (* x (vy v)) (* x (vz v))))
+	(vscale a (/ len))
+	a)))
 
-(define org car)
-(define dir cdr)
-(define new-ray cons)
+;;; ray
 
-;R6RS record version
-(define-record-type 
-  (intersection make-intersection intersection?)
-  (fields
-    (mutable t is-t is-t-set!)
-    (mutable p is-p is-p-set!)
-    (mutable n is-n is-n-set!)
-    (mutable col is-col is-col-set!)
-    (mutable emissiveCol is-emissiveCol is-emissiveCol-set!)
-    (mutable hit is-hit is-hit-set!))
-  (protocol
-    (lambda (c)
-      (lambda ()
-	(c 1.0e+30 (vec 0.0 0.0 0.0) (vec 0.0 0.0 0.0) (vec 0.0 0.0 0.0) (vec 0.0 0.0 0.0) #f)))))
+(define (make-ray org dir)
+  (cons org dir))
 
-(define (Sphere center radius col emissiveCol)
-  (lambda (isect ray)
-    (let* ((rs (vsub (org ray) center))
-	   (B (vdot rs (dir ray)))
-	   (C (- (vdot rs rs) (square radius)))
-	   (D (- (square B) C)))
-      (if (> D 0.0)
-	(let ((t (- 0.0 B (sqrt D))))
-	  (if (< 0.0 t (is-t isect))
-	    (begin
-	    (is-t-set! isect t)
-	    (is-hit-set! isect #t)
-	    (let ((p (vec 
-		       (+ (vx (org ray)) (* (vx (dir ray)) t))
-		       (+ (vy (org ray)) (* (vy (dir ray)) t))
-		       (+ (vz (org ray)) (* (vz (dir ray)) t)))))
-	      (is-n-set! isect (vnormalize (vsub p center)))
-	      (is-p-set! isect p))
-	    (is-col-set! isect col)
-	    (is-emissiveCol-set! isect emissiveCol))))))))
+(define (ray-org ray)
+  (car ray))
 
-(define (Plane p n col emissiveCol)
-  (lambda (isect ray)
-    (let ((d (- 0.0 (vdot p n)))
-	  (v (vdot (dir ray) n)))
-      (if (> (abs v) 1.0e-6)
-	(let ((t (/ (- 0.0 (+ (vdot (org ray) n) d)) v)))
-	  (if (< 0.0 t (is-t isect))
-	    (begin
-	    (is-hit-set! isect #t)
-	    (is-t-set! isect t)
-	    (is-n-set! isect n)
-	    (is-p-set! isect (vec
-			       (+ (vx (org ray)) (* (vx (dir ray)) t))
-			       (+ (vy (org ray)) (* (vy (dir ray)) t))
-			       (+ (vz (org ray)) (* (vz (dir ray)) t))))
-	    (is-col-set! isect col)
-	    (is-emissiveCol-set! isect emissiveCol))))))))
+(define (ray-dir ray)
+  (cdr ray))
 
-; pt
+;;; isect
 
-(define IMAGE_WIDTH 256)
-(define IMAGE_HEIGHT 256)
-(define NSUBSAMPLES 2)
-(define NPATH_SAMPLES 128)
-(define MAX_TRACE_DEPTH 16)
+(define (make-isect t p n hit)
+  (let ((v (make-vector 4)))
+    (vector-set! v 0 t)
+    (vector-set! v 1 p)
+    (vector-set! v 2 n)
+    (vector-set! v 3 hit)
+    v))
 
-(define (main)
-  (let ((myport (open-file-output-port "out.rgb")))
-  ;(let ()
-  (define (line-loop y)
-    (cond
-      ((= y IMAGE_HEIGHT)
-       (display "DONE")(newline))
-      (else
-	(let ((w (render IMAGE_WIDTH IMAGE_HEIGHT y NSUBSAMPLES)))
-	  (display "LINE")(display y)(display " - ")(display (bytevector-length w))(newline)
-	  (put-bytevector myport w))
-	(line-loop (+ y 1)))))
-  (line-loop 0)(close-port myport)))
+(define (isect-t i)
+  (vector-ref i 0))
 
-(define objects
-  (list
-    (Sphere (vec -1.05 0.0 -2.0) 0.5 (vec 0.75 0.0 0.0) (vec 0.0 0.0 0.0))
-    (Sphere (vec 0.0 0.0 -2.0) 0.5 (vec 1.0 1.0 1.0) (vec 1.0 1.0 1.0))
-    (Sphere (vec 1.05 0.0 -2.0) 0.5 (vec 0.0 0.0 1.0) (vec 0.0 0.0 0.0))
-    (Plane (vec 0.0 -0.5 0.0) (vec 0.0 1.0 0.0) (vec 1.0 1.0 1.0) (vec 0.0 0.0 0.0))))
+(define (isect-p i)
+  (vector-ref i 1))
+
+(define (isect-n i)
+  (vector-ref i 2))
+
+(define (isect-hit i)
+  (vector-ref i 3))
+
+;;; sphere
+
+(define (make-sphere center radius)
+  (let ((v (make-vector 2)))
+    (vector-set! v 0 center)
+    (vector-set! v 1 radius)
+    v))
+
+(define (sphere-center s)
+  (vector-ref s 0))
+
+(define (sphere-radius s)
+  (vector-ref s 1))
+
+;;; plane
+
+(define (make-plane p n)
+  (let ((v (make-vector 2)))
+    (vector-set! v 0 p)
+    (vector-set! v 1 n)
+    v))
+
+(define (plane-p p)
+  (vector-ref p 0))
+
+(define (plane-n p)
+  (vector-ref p 1))
+
+;;; objects
+
+(define spheres (make-vector 3))
+(define plane #f)
+
+(define (ray-sphere-intersect isect ray sphere)
+  (let ((rs (v- (ray-org ray)
+		(sphere-center sphere))))
+    (let* ((B (vdot rs (ray-dir ray)))
+	   (C (- (vdot rs rs) (square (sphere-radius sphere))))
+	   (D (- (* B B) C)))
+      (when (> D 0)
+	(let ((t (- .0 B (sqrt D))))
+	  (when (< .0 t (isect-t isect))
+	    (let ((hit 1)
+		  (p (v+ (ray-org ray)
+			 (vscale (ray-dir ray) t)))
+		  (n (v- (isect-p isect)
+			 (sphere-center sphere))))
+	      (set! isect (make-isect t p (vnormalize n) hit))))))
+      isect)))
+
+(define (ray-plane-intersect isect ray plane)
+  (let ((d (- (vdot (plane-p plane)
+		    (plane-n plane))))
+	(v (vdot (ray-dir ray)
+		 (plane-n plane))))
+    (when (> (abs v) 1.0e-17)
+      (let ((t (- (/ (+ (vdot (ray-org ray) (plane-n plane)) d) v))))
+	(when (< .0 t (isect-t isect))
+	  (let ((hit 1)
+		(p (v+ (ray-org ray)
+		       (vscale (ray-dir ray) t)))
+		(n (plane-n plane)))
+	    (set! isect (make-isect t p n hit))))))
+    isect))
+
+(define (orthobasis n)
+  (let ((v (cond
+	    ((< -0.6 (vx n) 0.6)
+	     (vec 1.0 0.0 0.0))
+	    ((< -0.6 (vy n) 0.6)
+	     (vec 0.0 1.0 0.0))
+	    ((< -0.6 (vz n) 0.6)
+	     (vec 0.0 0.0 1.0))
+	    (else
+	     (vec 1.0 0.0 0.0)))))
+    (let* ((s (vnormalize (vcross v n)))
+	   (t (vnormalize (vcross n s))))
+      (vector s t n))))
+
+(define (ambient-occlusion isect)
+  (let ((ntheta NAO-SAMPLES)
+	(nphi NAO-SAMPLES)
+	(eps 0.0001))
+    (let ((p (v+ (isect-p isect)
+		 (vscale (isect-n isect) eps)))
+	  (basis (orthobasis (isect-n isect)))
+	  (occlusion 0.0))
+      (dotimes (i (* ntheta nphi))
+	(let ((theta (sqrt (drand48)))
+	      (phi (* 2 PI (drand48))))
+	  (let ((x (* (cos phi) theta))
+		(y (* (sin phi) theta))
+		(z (sqrt (- 1.0 (square theta)))))
+	    (let ((rx (+ (* x (vx (vector-ref basis 0)))
+			 (* y (vx (vector-ref basis 1)))
+			 (* z (vx (vector-ref basis 2)))))
+		  (ry (+ (* x (vy (vector-ref basis 0)))
+			 (* y (vy (vector-ref basis 1)))
+			 (* z (vy (vector-ref basis 2)))))
+		  (rz (+ (* x (vz (vector-ref basis 0)))
+			 (* y (vz (vector-ref basis 1)))
+			 (* z (vz (vector-ref basis 2))))))
+	      (let ((ray (make-ray p (vec rx ry rz))))
+		(let ((occ-isect (make-isect 1.0e+17 (vec .0 .0 .0) (vec .0 .0 .0) 0)))
+		  (set! occ-isect
+			(ray-sphere-intersect occ-isect ray (vector-ref spheres 0)))
+		  (set! occ-isect
+			(ray-sphere-intersect occ-isect ray (vector-ref spheres 1)))
+		  (set! occ-isect
+			(ray-sphere-intersect occ-isect ray (vector-ref spheres 2)))
+		  (set! occ-isect
+			(ray-plane-intersect occ-isect ray plane))
+		  (when (not (zero? (isect-hit occ-isect)))
+		    (set! occlusion (+ occlusion 1)))))))))
+      (set! occlusion
+	    (/ (- (* ntheta nphi) occlusion)
+	       (* ntheta nphi)))
+      (vec occlusion occlusion occlusion))))
 
 (define (clamp f)
   (let ((i (exact (floor (* f 255.5)))))
@@ -137,87 +240,56 @@
       ((> i 255) 255)
       (else i))))
 
-(define (orthoBasis n)
-  (let* ((v
-	  (cond 
-	    ((< -0.6 (vx n) 0.6)
-	     (vec 1.0 0.0 0.0))
-	    ((< -0.6 (vy n) 0.6)
-	     (vec 0.0 1.0 0.0))
-	    ((< -0.6 (vz n) 0.6)
-	     (vec 0.0 0.0 1.0))
-	    (else (vec 1.0 0.0 0.0))))
-	(s (vnormalize (vcross v n))))
-    (values
-      s
-      (vnormalize (vcross n s))
-      n)))
+(define (render w h nsubsamples)
+  (let ((img (make-bytevector (* w h 3)))
+	(col (vec .0 .0 .0)))
+    (dotimes (y h)
+      (dotimes (x w)
+	(set! col (vec .0 .0 .0))
+	(dotimes (v nsubsamples)
+	  (dotimes (u nsubsamples)
+	    (let ((px (/ (+ x (/ u nsubsamples) (/ w -2.0))
+			 (/ w 2.0)))
+		  (py (- (/ (+ y (/ v nsubsamples) (/ h -2.0))
+			    (/ h 2.0)))))
+	      (let ((ray (make-ray (vec .0 .0 .0) (vnormalize (vec px py -1.0)))))
+		(let ((isect (make-isect 1.0e+17 (vec .0 .0 .0) (vec .0 .0 .0) 0)))
+		  (set! isect (ray-sphere-intersect isect ray (vector-ref spheres 0)))
+		  (set! isect (ray-sphere-intersect isect ray (vector-ref spheres 1)))
+		  (set! isect (ray-sphere-intersect isect ray (vector-ref spheres 2)))
+		  (set! isect (ray-plane-intersect isect ray plane))
+		  (when (not (zero? (isect-hit isect)))
+		    (set! col (v+ col (ambient-occlusion isect)))))))))
+	(set! col (vscale col (/ (square nsubsamples))))
+	(bytevector-u8-set! img (+ (* 3 (+ (* y w) x)) 0) (clamp (vx col)))
+	(bytevector-u8-set! img (+ (* 3 (+ (* y w) x)) 1) (clamp (vy col)))
+	(bytevector-u8-set! img (+ (* 3 (+ (* y w) x)) 2) (clamp (vz col)))))
+    img))
 
-(define (trace ray depth)
-  (define (find-nearest obj isect ray)
-    (cond
-      ((pair? obj) 
-       ((car obj) isect ray)
-       (find-nearest (cdr obj) isect ray))
-      (else isect)))
-  (let ((isect (find-nearest objects (make-intersection) ray)))
-  (if (is-hit isect)
-    ;true - stage2
-    (let ((r (random-real)) (phi (* 2.0 PI (random-real))))
-      (let 
-	((x (* (cos phi) (sqrt (- 1.0 r))))
-	 (y (* (sin phi) (sqrt (- 1.0 r))))
-	 (z (sqrt r)))
-	(receive (b0 b1 b2) (orthoBasis (is-n isect))
-	  (let*
-	    ((newDir (vec
-		       (+ (* x (vx b0)) (* y (vx b1)) (* z (vx b2)))
-		       (+ (* x (vy b0)) (* y (vy b1)) (* z (vy b2)))
-		       (+ (* x (vz b0)) (* y (vz b1)) (* z (vz b2)))))
-	     (eps 0.00001)
-	     (newP (vec (+ (vx (is-p isect)) (* eps (vx newDir)))
-			(+ (vy (is-p isect)) (* eps (vy newDir))) 
-			(+ (vz (is-p isect)) (* eps (vz newDir)))
-		     ))
-	     (newRay (new-ray newP newDir))
-	     (fr (vscale (is-col isect) (/ 1.0 PI)))
-	     (cosTheta (vdot newDir (is-n isect)))
-	     (Li (trace newRay (+ depth 1)))
-	     (Le (is-emissiveCol isect)))
-	    ;return
-	    (vec (+ (vx Le) (* PI (vx fr) (vx Li))) 
-		 (+ (vy Le) (* PI (vy fr) (vy Li))) 
-		 (+ (vz Le) (* PI (vz fr) (vz Li))))))))
-    (vec 0.7 0.7 0.7) ;false
-    )))
+(define (init-scene)
+  (vector-set! spheres 0 (make-sphere (vec -2.0 0.0 3.5) 0.5))
+  (vector-set! spheres 1 (make-sphere (vec -0.5 0.0 -3.0) 0.5))
+  (vector-set! spheres 2 (make-sphere (vec 1.0 0.0 -2.2) 0.5))
+  (set! plane (make-plane (vec 0.0 -0.5 0.0) (vec 0.0 1.0 0.0))))
 
-(define (pathTrace ray)
-  (define (pathTrace-itr num col ray)
-    (if (< num NPATH_SAMPLES)
-      (pathTrace-itr (+ num 1) (vadd col (trace ray 0)) ray)
-      (vscale col (/ 1.0 NPATH_SAMPLES))))
-  (pathTrace-itr 0 (vec 0.0 0.0 0.0) ray))
+(define (save-ppm filename w h img)
+  (with-output-to-file filename
+    (lambda ()
+      (write-string "P6\n")
+      (display w) (write-string " ") (display h) (newline)
+      (write-string "255\n")
+      (dotimes (y h)
+	(dotimes (x w)
+	  (display (bytevector-u8-ref img (+ (* 3 (+ (* y w) x)) 0)))
+	  (display " ")
+	  (display (bytevector-u8-ref img (+ (* 3 (+ (* y w) x)) 1)))
+	  (display " ")
+	  (display (bytevector-u8-ref img (+ (* 3 (+ (* y w) x)) 2)))
+	  (display "\n"))))))
 
-(define (render width height y nsubsamples)
-  (let ((img (make-bytevector (* width 4))))
-    (define (render-sample-loop i u v sumr sumg sumb)
-      (cond
-	((= v nsubsamples) (values sumr sumg sumb))
-	((= u nsubsamples) (render-sample-loop i 0 (+ v 1) sumr sumg sumb))
-	(else
-	  (let ((px (/ (+ i (- (/ u nsubsamples) (/ width 2.0))) (/ width 2.0)))
-		(py (- 0.0 (/ (+ y (- (/ v nsubsamples) (/ height 2.0))) (/ height 2.0)))))
-	    (let ((col (pathTrace (new-ray (vec 0.0 0.0 0.0) (vnormalize (vec px py -1.0))))))
-	      (render-sample-loop i (+ u 1) v (+ sumr (vx col)) (+ sumg (vy col)) (+ sumb (vz col))))))))
-
-    (define (render-pixel-loop i)
-      (if (= i width)
-	img
-	(begin
-	  (receive (sumr sumg sumb) (render-sample-loop i 0 0 0.0 0.0 0.0)
-		   (bytevector-u8-set! img (+ (* i 4) 0) (clamp (/ sumr (square nsubsamples))))
-		   (bytevector-u8-set! img (+ (* i 4) 1) (clamp (/ sumg (square nsubsamples))))
-		   (bytevector-u8-set! img (+ (* i 4) 2) (clamp (/ sumb (square nsubsamples)))))
-	  (render-pixel-loop (+ i 1))))) (render-pixel-loop 0)))
+(define (main)
+  (init-scene)
+  (let ((img (render WIDTH HEIGHT NSUBSAMPLES)))
+    (save-ppm "ao.ppm" WIDTH HEIGHT img)))
 
 (main)
